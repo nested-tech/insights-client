5a5a3d02fada72b807c5a8ed5e49e62374ed3c9e
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-08-14 14:04:54 +0100

    Tidy addresses and add more flow types

--- src/Addresses.js
+++ src/Addresses.js
@@ -1,32 +1,33 @@
 // @flow
 
 import React from "react";
+import type { Element } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
-export const Addresses = () => (
-  <Query
-    query={gql`
-      query addresses {
-        getAddresses {
-          id
-          postcode
-          houseNumber
-        }
-      }
-    `}
-  >
+const ADDRESS_LIST = gql`
+  query addresses {
+    getAddresses {
+      id
+      postcode
+      houseNumber
+    }
+  }
+`;
+
+export const Addresses = (): Element<any> => (
+  <Query query={ADDRESS_LIST}>
     {({ loading, error, data }) => {
       if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error :(</p>;
+      if (error) return <p>Could not get addresses :(</p>;
 
       return (
         <div>
-          <h2>Addresses</h2>
+          <h3>Addresses</h3>
           <ul>
             {data.getAddresses.map(({ postcode, houseNumber }) => (
               <li key={postcode}>
-                {houseNumber} - {postcode}
+                {houseNumber} {postcode}
               </li>
             ))}
           </ul>
